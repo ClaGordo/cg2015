e96f3d4a7b70b0e3ef9a10d35851fab55dd6c10c
claudio <claudiococciatelli@gmail.com>
2015-03-11 12:10:10 +0100

    Added exercise01, 02, 03 complete

--- 20150305/exercise02.js
+++ 20150305/exercise02.js
@@ -2,7 +2,9 @@ var s = "";
 for (i=1; i<=10; i++){
 	s = "";
 	for (j=1; j<=10; j++){
-		s += i*j + " ";
+		
+		s += i*j + "," + " ";
+
 	}
 	console.log(s);
 }
--- 20150305/exercise03.js
+++ 20150305/exercise03.js
@@ -2,7 +2,12 @@ var s = "";
 for (i=1; i<=10; i++){
 	s = "";
 	for (j=1; j<=10; j++){
-		s += i*j + " ";
+		if (i===j){
+			s += 1 + "," + " ";
+		}
+		else if (i!==j){
+			s += 0 + "," + " ";
+		}
 	}
 	console.log(s);
 }
